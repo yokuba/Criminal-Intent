78c8825b5751ba2f4ebaa5b382a1e7e8dd39df59
Alicia MacLennan <aflyingfemme@gmail.com>
2016-08-10 17:44:17 -0700

    Add crime class

--- app/src/main/java/com/aliciamaclennan/criminalintent/Crime.java
+++ app/src/main/java/com/aliciamaclennan/criminalintent/Crime.java
@@ -1,7 +1,27 @@
 package com.aliciamaclennan.criminalintent;
 
+import java.util.UUID;
+
 /**
  * Created by amac on 8/10/16.
  */
 public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
 }
