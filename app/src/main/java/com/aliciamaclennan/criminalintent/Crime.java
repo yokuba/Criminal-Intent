595422c4b1e10305abf70cd6e61b2edfc7b9be5e
Alicia MacLennan <aflyingfemme@gmail.com>
2016-08-12 13:32:04 -0700

    Add date and solved boolean to crime

--- app/src/main/java/com/aliciamaclennan/criminalintent/Crime.java
+++ app/src/main/java/com/aliciamaclennan/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.aliciamaclennan.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,9 +9,30 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
 
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
