0ec6e35eaa2bf3d6df76b9ff3bfcb5fd21a47e08
Alicia MacLennan <aflyingfemme@gmail.com>
2016-08-17 10:57:52 -0700

    Integrate crime pager activity

--- app/src/main/java/com/aliciamaclennan/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/aliciamaclennan/criminalintent/CrimePagerActivity.java
@@ -1,9 +1,51 @@
 package com.aliciamaclennan.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by amac on 8/17/16.
  */
 public class CrimePagerActivity extends FragmentActivity {
+    private static final String EXTRA_CRIME_ID = "com.aliciamaclennan.criminalintent.crime_id";
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
+
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+    }
 }
